b0d1b06ce2554de9bccafefd72045aa4fc0a7a9a
Bach Nguyen <bach20501@gmail.com>
2022-03-30 18:22:05 +0700

    Recommit

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -26,6 +26,7 @@ public class Bullet : MonoBehaviour
     {
         if (collision.collider.CompareTag("Enemy"))
         {
+            this.PostEvent(GameEvent.OnBulletCollideEnemy);
             collision.gameObject.SetActive(false);
             this.gameObject.SetActive(false);
         }
---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Coin : MonoBehaviour
+{
+    int numberOfCoins = 0;
+    public TextMeshProUGUI coinText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.RegisterListener(GameEvent.OnBulletCollideEnemy, OnBulletCollideEnemyHandler);
+    }
+
+    public void OnBulletCollideEnemyHandler(object obj)
+    {
+        numberOfCoins += 100;
+        coinText.text = numberOfCoins.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
